d3fa5027a1d51f8a36100f19c3c6a33fbc510e5c
Vartora <167640409+AMOUNAKAOA@users.noreply.github.com>
2026-02-18 16:06:45 +0100

    Create Program.cs file with chat application setup

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using MessageApp.Backend.Data;
+using MessageApp.Backend.Hubs;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Add database
+builder.Services.AddDbContext<ChatDbContext>(options =>
+    options.UseSqlite("Data Source=messaging_app.db");
+);
+
+// Add custom services
+builder.Services.AddScoped<ChatService>();
+
+// Add Swagger for API documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+app.MapHub<ChatHub>('/chatHub');
+
+// Create database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ChatDbContext>();
+    db.Database.EnsureCreated();
+}
+
+app.Run();
